9b49fed48e90a37a341a56ccfb8abbd4be4ef5df
Nancy Ihirwe <kayitarenancy@gmail.com>
2018-11-08 10:19:00 -0500

    restaurant rails

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,21 @@
 Rails.application.routes.draw do
+  get 'reviews/new'
+  get 'reviews/create'
+  get 'restaurants/index'
+  get 'restaurants/show'
+  get 'restaurants/new'
+  get 'restaurants/create'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :restaurants, only: [:index, :show, :new, :create] do
+    resources :reviews, only: [:new, :create]
+  end
+
+  # resources :restaurants do
+  #   resources :reviews, only: [:new, :create]
+  # end
+  # get 'restaurants', to: 'restaurants#index'
+  # get '/restaurants/new', to: 'restaurants#new'
+
+  # get '/restaurants/:id', to: 'restaurants#show', as: 'restaurant'
+  # post '/restaurants', to: 'restaurants#create'
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,46 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+puts 'Cleaning database...'
+Restaurant.destroy_all
+
+puts 'Creating restaurants...'
+restaurants_attributes = [
+  {
+    name:         'Wagamama',
+    address:      '7 Boundary St, London E2 7JE',
+    phone_number: '07507094655',
+    category:     'japanese'
+  },
+  {
+    name:         'sushi',
+    address:      '10 Boundary St, London E2 7JE',
+    phone_number: '07507094655',
+    category:     'chinese'
+  },
+  {
+    name:         'frite frite',
+    address:      '8 Boundary St, Paris E2 7JE',
+    phone_number: '07507094655',
+    category:     'belgian'
+  },
+  {
+    name:         'pain francée',
+    address:      '8 Boundary St, Bruxelles E2 7JE',
+    phone_number: '07507094655',
+    category:     'french'
+  },
+  {
+    name:         'pizza pizza',
+    address:      '8 Boundary St, Rome E2 7JE',
+    phone_number: '07507094655',
+    category:     'italian'
+  }
+]
+
+restaurants_attributes.each do |hash|
+  Restaurant.create!(hash)
+end
+
+puts 'Finished!'
